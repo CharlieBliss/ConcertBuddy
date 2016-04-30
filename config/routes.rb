a0461c286232d0788814980edb5ad6c3e51b3ceb
nataliecodes <nataliepolen@gmail.com>
2016-04-30 18:49:11 -0400

    Edit routes to allow for sessions#destory

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
+  #destory
   # You can have the root of your site routed with "root"
   root 'events#index'
 
