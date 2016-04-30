801bd79ed6ab220f250198b0ec39de0735089963
nataliecodes <nataliepolen@gmail.com>
2016-04-30 17:37:04 -0400

    Make events#index the home route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   post '/events/get_events', to: 'events#get_events'
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'events#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
