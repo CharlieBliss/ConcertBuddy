15cd8840cab13a17345c113fc2b83494647b6c1d
CharlieBliss <charles.m.bliss@gmail.com>
2016-04-30 16:50:54 -0400

    Bugfix until migrations work

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160430195927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "event_id",            null: false
+    t.string  "name",     limit: 50
+    t.integer "size",     limit: 2,  null: false
+    t.integer "owner_id",            null: false
+  end
+
+  add_index "groups", ["event_id"], name: "index_groups_on_event_id", using: :btree
+  add_index "groups", ["owner_id"], name: "index_groups_on_owner_id", using: :btree
+
+  create_table "groups_users", force: :cascade do |t|
+    t.integer  "group_id",   null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "groups_users", ["group_id"], name: "index_groups_users_on_group_id", using: :btree
+  add_index "groups_users", ["user_id"], name: "index_groups_users_on_user_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",            null: false
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
+    t.text     "about"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "users_tags", force: :cascade do |t|
+    t.integer  "tag_id",     null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "users_tags", ["user_id"], name: "index_users_tags_on_user_id", using: :btree
 
 end
