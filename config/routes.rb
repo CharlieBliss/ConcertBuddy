20044be31d8af530fcdc29f1ed1b736ef5f3f214
CharlieBliss <charles.m.bliss@gmail.com>
2016-04-30 19:01:24 -0400

    Update view page for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :events, only: [:index, :show]
+  resources :events, only: [:index, :show] do
+    resources :groups, only: [:new,:create,:index,:show]
+  end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
