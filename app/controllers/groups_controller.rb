af3cebe4fa4be570696359f706bdf1c0ca482c2d
nataliecodes <nataliepolen@gmail.com>
2016-05-05 16:44:08 -0400

    Add @event to groups/new route and add groups/show route

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,10 +2,15 @@ class GroupsController < ApplicationController
 
   def new
     @group = Group.new
+    @event = Event.find_by(id: params[:event_id])
   end
 
   def create
 
+  end
+
+  def show
+    @group = Group.find_by(id: params[:group_id])
   end
 
 end
