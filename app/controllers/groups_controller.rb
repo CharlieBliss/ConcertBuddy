3d1e19298140783595f19b953d40b883021cddd6
nataliecodes <nataliepolen@gmail.com>
2016-05-05 17:30:08 -0400

    Add groups#create route in controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,12 +6,23 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
-    puts 'hey'
+    @group = Group.new(group_params)
+    @group.event = Event.find_by(id: params[:event_id])
+    if @group.save
+      redirect_to event_group_path(@group, @group.event)
+    else
+      binding.pry
+    end
   end
 
   def show
     @group = Group.find_by(id: params[:group_id])
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :size).merge(owner: current_user)
   end
 
 end
