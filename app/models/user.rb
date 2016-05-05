579acd3c3cc27684659968fbac6ae7e564e82b59
CharlieBliss <charles.m.bliss@gmail.com>
2016-05-05 17:36:06 -0400

    Add logic to send new users to a profile completion page(under construction)

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   validates :about, length: {maximum: 500}
 
   def self.sign_in_from_omniauth(auth)
-    find_by(provider: auth['provider'], uid: auth['uid']) || create_user_from_omni_auth(auth)
+    find_by(provider: auth['provider'], uid: auth['uid'])
   end
 
   def self.create_user_from_omni_auth(auth)
