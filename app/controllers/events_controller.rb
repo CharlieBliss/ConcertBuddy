0f4707b4556f1acea981411343ac3b9b4e2e6d73
thomas-yancey <tomyncy618@gmail.com>
2016-05-06 16:52:28 -0400

    add infinite scroll, clean up api call to eventbrite

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,32 +1,22 @@
 class EventsController < ApplicationController
 
-  # def get_events
-  #   # @event = Event.find(params[:id])
-
-  #   jsondata = JSON.parse(params[:data])
-  #   jsondata.each do |event|
-  #     event = Event.new(url: event["url"], name: event["name"], start: event["start"], address: event["address"], eventbrite_id: event["id"])
-  #     event.save
-  #   end
-  # end
-
-  # def index
-  #   binding.pry
-  # end
-
-
   def show
     @event = Event.find(params[:id])
     @groups = @event.groups
   end
 
   def index
-    # num_photos = 10
-    # 'https://www.eventbriteapi.com/v3/events/search/?token='+token+'&venue.city=new+york&expand=venue'
-    # # "https://www.eventbriteapi.com/v3/events/search/?token='+ZRZ2ELU45C2X5UVCKE"
-    # binding.pry
-    # will eventually be the events for that user on this call based on location
-    @events = Event.all
+
+    @events = Event.paginate(page: params[:page], per_page: 15).where('start >= ?',Time.now).order('start ASC')
+
+    if request.xhr?
+      render @events
+    end
+
+  end
+
+  def paginate_events
+    binding.pry
   end
 
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ActiveRecord::Base
   # validates :title, :location, presence: true
-  validates_uniqueness_of :eventbrite_id
+  validates_uniqueness_of :event_id, scope: :provider_id
 
   has_and_belongs_to_many :users
   has_many :groups
+
 end
