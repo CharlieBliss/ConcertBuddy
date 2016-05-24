bdd8bb6ecec4b3d7e22ee3169420112450c3013e
thomas-yancey <tomyncy618@gmail.com>
2016-05-24 18:27:48 -0400

    remove load wheel, change email port

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -38,7 +38,7 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.event = Event.find_by(id: params[:event_id])
     if @group.save
-        UserMailer.new_group_email(@group.owner,@group).deliver_now
+      UserMailer.new_group_email(@group.owner,@group).deliver_now
       redirect_to user_path(@current_user)
     else
       flash[:notice] = "There was an error saving your group. Please try again."
